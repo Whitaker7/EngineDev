aa1032f5cbd955fff532be685bae3ca3411f04d4
Whitaker Antalek <wsantalek@student.fullsail.edu>
2022-11-27 21:29:46 -0500

    grid changes color

--- MemoryManagement/MemoryManagement/Renderer/Renderer/dev_app.cpp
+++ MemoryManagement/MemoryManagement/Renderer/Renderer/dev_app.cpp
@@ -2,6 +2,7 @@
 #include "math_types.h"
 #include "debug_renderer.h"
 #include <iostream>
+#include "../../pools.h"
 //TODO include debug_renderer.h and pools.h and anything else you might need here
 
 namespace end
@@ -36,69 +37,70 @@ namespace end
 		delta_time = calc_delta_time();
 
 		//This drawn the green checkmark
-		end::debug_renderer::add_line(float3(-2, 0, 0), float3(0, -3, 0), color);
-		end::debug_renderer::add_line(float3(0, -3, 0), float3(3, 4, 0), color);
+		/*end::debug_renderer::add_line(float3(-2, 0, 0), float3(0, -3, 0), color);
+		end::debug_renderer::add_line(float3(0, -3, 0), float3(3, 4, 0), color);*/
+		 
+		//this draws the grid horizontal
+		end::debug_renderer::add_line(float3(-10, 0, 10), float3(10, 0, 10), color);
+		end::debug_renderer::add_line(float3(-10, 0, 9), float3(10, 0, 9), color);
+		end::debug_renderer::add_line(float3(-10, 0, 8), float3(10, 0, 8), color);
+		end::debug_renderer::add_line(float3(-10, 0, 7), float3(10, 0, 7), color);
+		end::debug_renderer::add_line(float3(-10, 0, 6), float3(10, 0, 6), color);
+		end::debug_renderer::add_line(float3(-10, 0, 5), float3(10, 0, 5), color);
+		end::debug_renderer::add_line(float3(-10, 0, 4), float3(10, 0, 4), color);
+		end::debug_renderer::add_line(float3(-10, 0, 3), float3(10, 0, 3), color);
+		end::debug_renderer::add_line(float3(-10, 0, 2), float3(10, 0, 2), color);
+		end::debug_renderer::add_line(float3(-10, 0, 1), float3(10, 0, 1), color);
+		end::debug_renderer::add_line(float3(-10, 0, 0), float3(10, 0, 0), color);
+		end::debug_renderer::add_line(float3(-10, 0, -10), float3(10, 0, -10), color);
+		end::debug_renderer::add_line(float3(-10, 0, -9), float3(10, 0, -9), color);
+		end::debug_renderer::add_line(float3(-10, 0, -8), float3(10, 0, -8), color);
+		end::debug_renderer::add_line(float3(-10, 0, -7), float3(10, 0, -7), color);
+		end::debug_renderer::add_line(float3(-10, 0, -6), float3(10, 0, -6), color);
+		end::debug_renderer::add_line(float3(-10, 0, -5), float3(10, 0, -5), color);
+		end::debug_renderer::add_line(float3(-10, 0, -4), float3(10, 0, -4), color);
+		end::debug_renderer::add_line(float3(-10, 0, -3), float3(10, 0, -3), color);
+		end::debug_renderer::add_line(float3(-10, 0, -2), float3(10, 0, -2), color);
+		end::debug_renderer::add_line(float3(-10, 0, -1), float3(10, 0, -1), color);
+		end::debug_renderer::add_line(float3(-10, 0, -0), float3(10, 0, -0), color);
 
-		////this draws the grid horizontal
-		//end::debug_renderer::add_line(float3(-10, 0, 10), float3(10, 0, 10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 9), float3(10, 0, 9), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 8), float3(10, 0, 8), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 7), float3(10, 0, 7), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 6), float3(10, 0, 6), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 5), float3(10, 0, 5), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 4), float3(10, 0, 4), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 3), float3(10, 0, 3), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 2), float3(10, 0, 2), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 1), float3(10, 0, 1), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 0), float3(10, 0, 0), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -10), float3(10, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -9), float3(10, 0, -9), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -8), float3(10, 0, -8), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -7), float3(10, 0, -7), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -6), float3(10, 0, -6), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -5), float3(10, 0, -5), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -4), float3(10, 0, -4), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -3), float3(10, 0, -3), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -2), float3(10, 0, -2), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -1), float3(10, 0, -1), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, -0), float3(10, 0, -0), float4(0.1f, 1, 1, 1));
-
-		////this draws the grid vertical
-		//end::debug_renderer::add_line(float3(10, 0, 10), float3(10, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(9, 0, 10), float3(9, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(8, 0, 10), float3(8, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(7, 0, 10), float3(7, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(6, 0, 10), float3(6, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(5, 0, 10), float3(5, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(4, 0, 10), float3(4, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(3, 0, 10), float3(3, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(2, 0, 10), float3(2, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(1, 0, 10), float3(1, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(0, 0, 10), float3(0, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-1, 0, 10), float3(-1, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-2, 0, 10), float3(-2, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-3, 0, 10), float3(-3, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-4, 0, 10), float3(-4, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-5, 0, 10), float3(-5, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-6, 0, 10), float3(-6, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-7, 0, 10), float3(-7, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-8, 0, 10), float3(-8, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-9, 0, 10), float3(-9, 0, -10), float4(0.1f, 1, 1, 1));
-		//end::debug_renderer::add_line(float3(-10, 0, 10), float3(-10, 0, -10), float4(0.1f, 1, 1, 1));
+		//this draws the grid vertical
+		end::debug_renderer::add_line(float3(10, 0, 10), float3(10, 0, -10), color);
+		end::debug_renderer::add_line(float3(9, 0, 10), float3(9, 0, -10), color);
+		end::debug_renderer::add_line(float3(8, 0, 10), float3(8, 0, -10), color);
+		end::debug_renderer::add_line(float3(7, 0, 10), float3(7, 0, -10), color);
+		end::debug_renderer::add_line(float3(6, 0, 10), float3(6, 0, -10), color);
+		end::debug_renderer::add_line(float3(5, 0, 10), float3(5, 0, -10), color);
+		end::debug_renderer::add_line(float3(4, 0, 10), float3(4, 0, -10), color);
+		end::debug_renderer::add_line(float3(3, 0, 10), float3(3, 0, -10), color);
+		end::debug_renderer::add_line(float3(2, 0, 10), float3(2, 0, -10), color);
+		end::debug_renderer::add_line(float3(1, 0, 10), float3(1, 0, -10), color);
+		end::debug_renderer::add_line(float3(0, 0, 10), float3(0, 0, -10), color);
+		end::debug_renderer::add_line(float3(-1, 0, 10), float3(-1, 0, -10), color);
+		end::debug_renderer::add_line(float3(-2, 0, 10), float3(-2, 0, -10), color);
+		end::debug_renderer::add_line(float3(-3, 0, 10), float3(-3, 0, -10), color);
+		end::debug_renderer::add_line(float3(-4, 0, 10), float3(-4, 0, -10), color);
+		end::debug_renderer::add_line(float3(-5, 0, 10), float3(-5, 0, -10), color);
+		end::debug_renderer::add_line(float3(-6, 0, 10), float3(-6, 0, -10), color);
+		end::debug_renderer::add_line(float3(-7, 0, 10), float3(-7, 0, -10), color);
+		end::debug_renderer::add_line(float3(-8, 0, 10), float3(-8, 0, -10), color);
+		end::debug_renderer::add_line(float3(-9, 0, 10), float3(-9, 0, -10), color);
+		end::debug_renderer::add_line(float3(-10, 0, 10), float3(-10, 0, -10), color);
 
 
 		//TODO do you Updates here
 
 		//change a color over time
+#pragma region change line color
 		if (colorChange == true)
 		{
-			color.x -= 0.15f * delta_time;
-			color.y += 0.15f * delta_time;
+			color.x -= 0.25f * delta_time;
+			color.y += 0.25f * delta_time;
 		}
 		else
 		{
-			color.x += 0.15f * delta_time;
-			color.y -= 0.15f * delta_time;
+			color.x += 0.25f * delta_time;
+			color.y -= 0.25f * delta_time;
 		}
 
 		if (color.x > 1)
@@ -109,5 +111,7 @@ namespace end
 		{
 			colorChange = false;
 		}
+#pragma endregion
+
 	}
 }
